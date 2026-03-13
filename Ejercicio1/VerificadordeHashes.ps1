e69a25d18054e0582b39a7631715e3d37856de2c
Falco-art <francobenitez.science@gmail.com>
2026-03-13 14:35:51 -0600

    Create VerificadordeHashes.ps1

---
+++ Ejercicio1/VerificadordeHashes.ps1
@@ -0,0 +1,12 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RutadelArchivo
+)
+
+if (Test-Path $RutadelArchivo) {
+    Write-Host "Calculando Hashes para el archivo..." -ForegroundColor Cyan
+    Get-FileHash -Path $RutadelArchivo -Algorithm SHA256
+    Get-FileHash -Path $RutadelArchivo -Algorithm MD5
+} else {
+    Write-Host "Error: El archivo no existe en la ruta especificada." -ForegroundColor Red
+}
